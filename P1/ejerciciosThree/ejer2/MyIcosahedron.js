827153b7fbe17b84d693866718964be9e5918d59
Gsandoval96 <gsandoval@correo.ugr.es>
2020-03-02 19:01:46 +0100

    Trasteando con ejercicio 2

---
+++ P1/ejerciciosThree/ejer2/MyIcosahedron.js
@@ -0,0 +1,56 @@
+
+class MyIcosahedron extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+
+    // Se crea la parte de la interfaz que corresponde a la caja
+    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
+    this.createGUI(gui,titleGui);
+    // Un Mesh se compone de geometría y material
+    var icosahedronGeom = new THREE.IcosahedronGeometry( 1.0);
+    // Como material se crea uno a partir de un color
+    var icosahedronMat = new THREE.MeshPhongMaterial({color: 0x00CFCF});
+
+    // Ya podemos construir el Mesh
+    this.icosahedron = new THREE.Mesh (icosahedronGeom, icosahedronMat);
+    // Y añadirlo como hijo del Object3D (el this)
+    this.add(this.icosahedron);
+  }
+
+  createGUI (gui,titleGui) {
+    // Controles para el tamaño, la orientación y la posición de la caja
+    this.guiControls = new function () {
+      this.radius = 1.0;
+
+      // Un botón para dejarlo todo en su posición inicial
+      // Cuando se pulse se ejecutará esta función.
+      this.reset = function () {
+        this.radius = 1.0;
+      }
+    }
+
+    // Se crea una sección para los controles de la caja
+    var folder = gui.addFolder (titleGui);
+    var that = this;
+    // Estas lineas son las que añaden los componentes de la interfaz
+    // Las tres cifras indican un valor mínimo, un máximo y el incremento
+    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
+    folder.add (this.guiControls, 'radius', 0.1, 5.0, 0.1).name ('Radio : ').listen();
+    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
+  }
+
+  update () {
+    this.rotation.y += 0.01;
+    // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
+    // Primero, el escalado
+    // Segundo, la rotación en Z
+    // Después, la rotación en Y
+    // Luego, la rotación en X
+    // Y por último la traslación
+    //this.position.set (this.guiControls.posX,this.guiControls.posY,this.guiControls.posZ);
+    //this.rotation.set (this.guiControls.rotX,this.guiControls.rotY,this.guiControls.rotZ);
+    this.scale.set(this.guiControls.radius,this.guiControls.radius,this.guiControls.radius);
+  }
+
+
+}
---
+++ P1/ejerciciosThree/ejer2/MySphere.js
@@ -0,0 +1,66 @@
+
+class MySphere extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+
+    // Se crea la parte de la interfaz que corresponde a la caja
+    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
+    this.createGUI(gui,titleGui);
+    // Un Mesh se compone de geometría y material
+    var sphereGeom = new THREE.SphereGeometry( 0.5, 20.0, 20.0);
+    // Como material se crea uno a partir de un color
+    var sphereMat = new THREE.MeshPhongMaterial({color: 0x0000CF});
+
+    // Ya podemos construir el Mesh
+    this.sphere = new THREE.Mesh (sphereGeom, sphereMat);
+    // Y añadirlo como hijo del Object3D (el this)
+    this.add(this.sphere);
+  }
+
+  createGUI (gui,titleGui) {
+    // Controles para el tamaño, la orientación y la posición de la caja
+    this.guiControls = new function () {
+      this.radius = 0.5;
+      this.segments = 20.0;
+
+      // Un botón para dejarlo todo en su posición inicial
+      // Cuando se pulse se ejecutará esta función.
+      this.reset = function () {
+        this.radius = 0.5;
+        this.segments = 20.0;
+      }
+    }
+
+    // Se crea una sección para los controles de la caja
+    var folder = gui.addFolder (titleGui);
+    var that = this;
+    // Estas lineas son las que añaden los componentes de la interfaz
+    // Las tres cifras indican un valor mínimo, un máximo y el incremento
+    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
+    folder.add (this.guiControls, 'radius', 0.1, 5.0, 0.1).name ('Radio : ').listen();
+    folder.add (this.guiControls, 'segments', 3.0, 60.0, 1.0).name ('Nº Segmentos : ').onChange(function(value){that.crearNueva()});
+    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
+  }
+
+  crearNueva(){
+    var sphereGeom = new THREE.SphereGeometry( this.guiControls.radius,this.guiControls.segments, this.guiControls.segments);
+    this.sphere.geometry = sphereGeom;
+    console.log(this.guiControls.radius);
+    console.log(this.guiControls.segments);
+  }
+
+  update () {
+    this.rotation.y += 0.01;
+    // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
+    // Primero, el escalado
+    // Segundo, la rotación en Z
+    // Después, la rotación en Y
+    // Luego, la rotación en X
+    // Y por último la traslación
+    //this.position.set (this.guiControls.posX,this.guiControls.posY,this.guiControls.posZ);
+    //this.rotation.set (this.guiControls.rotX,this.guiControls.rotY,this.guiControls.rotZ);
+    this.scale.set(2*this.guiControls.radius,2*this.guiControls.radius,2*this.guiControls.radius);
+  }
+
+
+}
