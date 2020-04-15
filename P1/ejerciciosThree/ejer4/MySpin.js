1c50d1b2920e46707e75d843ae72528c272fcb8b
Gsandoval96 <gsandoval@correo.ugr.es>
2020-04-15 19:32:30 +0200

    Ejercicio 4 con botón animación

--- P1/ejerciciosThree/ejer4/MySpin.js
+++ P1/ejerciciosThree/ejer4/MySpin.js
@@ -41,50 +41,35 @@ class MySpin extends THREE.Object3D {
 
     createGUI (gui,titleGui) {
 
-    var that = this;
-
-    // Controles para el tamaño, la orientación y la posición de la caja
-    this.guiControls = new function () {
-      this.segments = 12.0;
-      this.radius = 1.0;
-
-      // Un botón para dejarlo todo en su posición inicial
-      // Cuando se pulse se ejecutará esta función.
-      this.reset = function () {
-        this.segments = 12.0;
-        this.radius = 1.0;
-        that.crearNueva(this.segments, this.radius);
-        console.log("HOLA");
+      // La escena le va a añadir sus propios controles.
+      // Se definen mediante una   new function()
+      // En este caso la intensidad de la luz y si se muestran o no los ejes
+      this.guiControls = new function() {
+        this.animacionOnOff = true;
       }
-    }
 
-    // Se crea una sección para los controles de la caja
-    var folder = gui.addFolder (titleGui);
-    // Estas lineas son las que añaden los componentes de la interfaz
-    // Las tres cifras indican un valor mínimo, un máximo y el incremento
-    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
-    folder.add (this.guiControls, 'segments', 3.0, 20.0, 1.0).name ('Nº Segmentos : ').onChange(function(value){that.crearNueva(that.guiControls.segments,that.guiControls.radius)});
-    folder.add (this.guiControls, 'radius', -0.001, 1, 0.001).name ('Radio : ').onChange(function(value){that.crearNueva(that.guiControls.segments, that.guiControls.radius)});
-    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
-  }
+      // Se crea una sección para los controles de esta clase
+      var folder = gui.addFolder (titleGui);
+      // Y otro para mostrar u ocultar los ejes
+      folder.add (this.guiControls, 'animacionOnOff').name ('Animación activa : ');
 
-  crearNueva(segmentos, radio){
-    var trebolGeom = new THREE.LatheGeometry( this.points, segmentos, 0, radio*2*Math.PI );
-    this.trebol.geometry = trebolGeom;
+      return gui;
   }
 
   update () {
 
-    this.rotation.z += 0.01;
+    if (this.guiControls.animacionOnOff){
+      this.rotation.z += 0.01;
 
-    this.diamanteCD.rotation.z -= 0.01;
-    this.corazonCD.rotation.z -= 0.01;
-    this.trebolCD.rotation.z -= 0.01;
-    this.picaCD.rotation.z -= 0.01;
+      this.diamanteCD.rotation.z -= 0.01;
+      this.corazonCD.rotation.z -= 0.01;
+      this.trebolCD.rotation.z -= 0.01;
+      this.picaCD.rotation.z -= 0.01;
 
-    this.diamanteAB.rotation.y += 0.01;
-    this.corazonAB.rotation.y += 0.01;
-    this.trebolAB.rotation.y += 0.01;
-    this.picaAB.rotation.y += 0.01;
+      this.diamanteAB.rotation.y += 0.01;
+      this.corazonAB.rotation.y += 0.01;
+      this.trebolAB.rotation.y += 0.01;
+      this.picaAB.rotation.y += 0.01;
+    }
   }
 }
