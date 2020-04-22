7f669a4e7f076c188ad61826f5741f5b3d92e7cf
Gsandoval96 <gsandoval@correo.ugr.es>
2020-04-22 20:32:06 +0200

    Ejercicio 10B terminado

--- P1/ejerciciosThree/ejercicio_10_b/MySaltarina.js
+++ P1/ejerciciosThree/ejercicio_10_b/MySaltarina.js
@@ -6,6 +6,11 @@ class MySaltarina extends THREE.Object3D {
 
     this.createGUI(gui);
 
+    this.momento = 0;
+
+    this.variacion = new THREE.Vector2(0,20);
+    this.tiempo = new THREE.Vector2(0,1);
+
     this.subiendo = true;
 
     this.tiempoAnterior = Date.now();
@@ -29,7 +34,6 @@ class MySaltarina extends THREE.Object3D {
 
     this.add(this.pelotaC);
 
-
   }
 
   createGUI (gui) {
@@ -44,7 +48,7 @@ class MySaltarina extends THREE.Object3D {
     // Estas lineas son las que añaden los componentes de la interfaz
     // Las tres cifras indican un valor mínimo, un máximo y el incremento
     // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
-    folder.add (this.guiControls, 'radius', 5, 30.0, 0.1).name ('Radio : ').onChange(function(value){that.crearNueva()});
+    folder.add (this.guiControls, 'radius', 5, 20.0, 0.1).name ('Radio : ').onChange(function(value){that.crearNueva()});
   }
 
   crearNueva(){
@@ -52,23 +56,36 @@ class MySaltarina extends THREE.Object3D {
     this.cylinder.geometry = cylinderGeom;
   }
 
+  funcionAD(landa){
+    return -2 * landa*landa*landa + 3 * landa*landa;
+  }
+
+  valor(t){
+    var landa = ( t - this.tiempo.x ) / ( this.tiempo.y - this.tiempo.x );
+    var valor = this.variacion.x + this.funcionAD(landa) * (this.variacion.y - this.variacion.x);
+    return valor;
+  }
+
   update () {
     this.cylinder.position.y = 10;
+
     this.pelotaA.position.x = this.guiControls.radius;
-
-    if(this.pelotaC.position.y < 20 && this.subiendo){
-      this.pelotaC.position.y += 0.4;
-      if(this.pelotaC.position.y >= 20) this.subiendo = false;
-    }
-    else if(this.pelotaC.position.y > 0 && !this.subiendo){
-      this.pelotaC.position.y -= 0.4;
-      if(this.pelotaC.position.y < 0) this.subiendo = true;
-    }
-
 
     var tiempoActual = Date.now();
     var segundos = (tiempoActual - this.tiempoAnterior)/1000;
     this.pelotaB.rotation.y  += segundos * this.velocidadRotacion;
+
+    this.pelotaB.position.y = this.valor(this.momento);
+
+    if(this.momento < this.tiempo.y && this.subiendo){
+      this.momento += segundos;
+      if(this.momento >= this.tiempo.y) this.subiendo = false;
+    }
+    else{
+      this.momento -= segundos;
+      if(this.momento <= this.tiempo.x) this.subiendo = true;
+    }
+
     this.tiempoAnterior = tiempoActual;
   }
 }
