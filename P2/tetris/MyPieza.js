0e5e4989d2fafe538ed709aac34ba95ab197d5d5
Gsandoval96 <gsandoval@correo.ugr.es>
2020-05-06 20:50:31 +0200

    Generador de piezas aleatorio hecho

--- P2/tetris/MyPieza.js
+++ P2/tetris/MyPieza.js
@@ -1,21 +1,77 @@
-
-
 class MyPieza extends THREE.Object3D {
+
   constructor() {
     super();
 
+    this.tipo = this.elegirTipo();
 
     this.material = new THREE.MeshStandardMaterial({color: 0xFF0000});
     this.pos = new THREE.Vector3(1,1,0);
-    this.perifs = new THREE.Vector3(new THREE.Vector2(0,1),
-                                    new THREE.Vector2(0,-1),
-                                    new THREE.Vector2(1,-1));
+
+    this.perifs = this.crearPerifs();
 
     this.pieza = this.crearPieza();
 
     this.add(this.pieza);
+  }
 
+  elegirTipo(){
+    var tipos = ['L','J','S','Z','T','I','O'];
+    var valor = Math.round(Math.random()*6);
 
+    console.log(tipos[valor]);
+
+    return tipos[valor];
+  }
+
+  crearPerifs(){
+    var perif;
+
+    switch (this.tipo){
+      case 'L':
+        perif = new THREE.Vector3(new THREE.Vector2(0,1),
+                                      new THREE.Vector2(0,-1),
+                                      new THREE.Vector2(1,-1));
+      break;
+
+      case 'J':
+        perif = new THREE.Vector3(new THREE.Vector2(0,1),
+                                      new THREE.Vector2(0,-1),
+                                      new THREE.Vector2(-1,-1));
+      break;
+
+      case 'S':
+        perif = new THREE.Vector3(new THREE.Vector2(-1,0),
+                                      new THREE.Vector2(0,1),
+                                      new THREE.Vector2(1,1));
+      break;
+
+      case 'Z':
+        perif = new THREE.Vector3(new THREE.Vector2(1,0),
+                                      new THREE.Vector2(0,1),
+                                      new THREE.Vector2(-1,1));
+      break;
+
+      case 'T':
+        perif = new THREE.Vector3(new THREE.Vector2(0,1),
+                                      new THREE.Vector2(1,0),
+                                      new THREE.Vector2(-1,0));
+      break;
+
+      case 'I':
+        perif = new THREE.Vector3(new THREE.Vector2(0,1),
+                                      new THREE.Vector2(0,-1),
+                                      new THREE.Vector2(0,2));
+      break;
+
+      case 'O':
+        perif = new THREE.Vector3(new THREE.Vector2(0,1),
+                                      new THREE.Vector2(1,1),
+                                      new THREE.Vector2(1,0));
+      break;
+    }
+
+    return perif;
   }
 
   crearPieza(){
